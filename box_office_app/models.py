fe686be7b2ef4f5fbb5381e7dd41f8e35e283b08
parkersingh <rekrapsingh@gmail.com>
2021-06-19 12:58:24 -0700

    Create login/registration

--- box_office_app/models.py
+++ box_office_app/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from datetime import datetime, timedelta
 
-# Create your models here.
+import bcrypt
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+# UserManager overrides User.objects
+class UserManager(models.Manager):
+    # validator validates all registration data for new user
+    def validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = "First name should be at least 2 characters"
+        
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = "Last name should be at least 2 characters"
+
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = "Invalid email address"
+        
+        if len(postData['password']) < 8:
+            errors['password'] = "Password should be at least 8 characters"
+        
+        if postData['password'] != postData['confirm_pw']:
+            errors['password'] = "Passwords do not match"
+
+        return errors
+    
+    # register takes registration data and creates a new User from that data
+    def register(self, postData):
+        pw = bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt()).decode()
+
+        return User.objects.create(
+            first_name = postData['first_name'],
+            last_name = postData['last_name'],
+            email = postData['email'],
+            password = pw
+        )
+    
+    # authenticate logs in existing users
+    def authenticate(self, email, password):
+        users = User.objects.filter(email= email)
+        if users:
+            user = users[0]
+            if bcrypt.checkpw(password.encode(), user.password.encode()):
+                return True
+            else:
+                return False
+        return False
+
+class User(models.Model):
+    first_name = models.CharField(max_length= 255)
+    last_name = models.CharField(max_length= 255)
+    email = models.EmailField(unique= True)
+    password = models.CharField(max_length= 255)
+    created_at = models.DateTimeField(auto_now_add= True)
+    updated_at = models.DateTimeField(auto_now= True)
+    objects = UserManager()
+
--- box_office_app/views.py
+++ box_office_app/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+# create creates new user only with POST status and sends
+# new user to dashboard or returns any errors made while registering
+def create(request):
+    if request.method == 'GET':
+        return redirect('/')
+
+    errors = User.objects.validator(request.POST)
+    if errors:
+        for e in errors.values():
+            messages.error(request, e)
+        return redirect('/')
+    else:
+        new_user = User.objects.register(request.POST)
+        request.session['user_id'] = new_user.id
+        return redirect('/dashboard')
+
+# logs existing users in to dashboard only with POST status
+def login(request):
+    if request.method == 'GET':
+        return redirect('/')
+
+    if not User.objects.authenticate(request.POST['email'], request.POST['password']):
+        messages.error(request, 'Invalid Email/Password')
+        return redirect('/')
+
+    user = User.objects.get(email= request.POST['email'])
+    request.session['user_id'] = user.id
+
+    return redirect('/dashboard')
+
+# logout logs user out of app and returns to register/login page
+def logout(request):
+    request.session.flush()
+    return redirect('/')
